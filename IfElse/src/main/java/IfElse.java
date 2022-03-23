e937fb3c20829c4a5bf7eed8d466e532a585aa66
Marcelo <cheroxmiller@gmail.com>
2022-03-22 23:03:09 -0300

    inclusão do projeto

---
+++ IfElse/src/main/java/IfElse.java
@@ -0,0 +1,18 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int idadeMedia = 18;
+		int minhaIdade = 17;
+		
+		if (minhaIdade>=idadeMedia) {
+			System.out.println("Apto para habilitação, sua idade é " + minhaIdade + ".");
+		}else {
+			System.err.println("Inapto para habilitação, sua idade é " + minhaIdade + ".") ;
+		}
+	}
+
+}
